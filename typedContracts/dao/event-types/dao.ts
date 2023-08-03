8a981d99c3ec0e01f7105906af713ca938014514
EmilKietzman <135075052+EmilKietzman@users.noreply.github.com>
2023-08-03 23:16:56 +0200

    code-assigment-1

---
+++ typedContracts/dao/event-types/dao.ts
@@ -0,0 +1,3 @@
+import type {ReturnNumber} from "@brushfam/typechain-types";
+import type * as ReturnTypes from '../types-returns/dao';
+
---
+++ typedContracts/governance-token/event-types/my_psp22_metadata.ts
@@ -0,0 +1,3 @@
+import type {ReturnNumber} from "@brushfam/typechain-types";
+import type * as ReturnTypes from '../types-returns/my_psp22_metadata';
+
